a6b5e1ef38a2172fb342fa6c4a0c6fe6b120a934
Juan Palomino <Jpalomino09@gmail.com>
2019-07-10 20:00:30 -0700

    gif img switching implemented

--- assets/javascript/giphylogic.js
+++ assets/javascript/giphylogic.js
@@ -11,31 +11,42 @@ $(document).on("click", ".gif-button", function(event) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response)
+    console.log(response);
     var results = response.data;
     $("#gifs-appear-here").html("");
+
     for (var i = 0; i < results.length; i++) {
-      var gifDiv = $("<div>");
-      gifDiv.addClass("gif");
-      gifDiv.addClass("col-md-3");
-
+      let gifDiv = $("<div>");
+      let gifImg = $("<img>");
       var rating = results[i].rating;
-      let stillImg = results[i].images["480w_still"].url
-
+      let stillImg = results[i].images["480w_still"].url;
+      let gif = results[i].images.fixed_width.url;
       var p = $("<p>").text("Rating: " + rating);
 
-      var reactImage = $("<img>");
-      reactImage.attr("src", results[i].images.fixed_width.url);
+      gifDiv.addClass("gif");
+      gifDiv.addClass("col-md-3");
       gifDiv.prepend(p);
-      gifDiv.prepend(reactImage);
-      
-      $(".gif").click(()=>{
-        console.log("image clicked");
-        $(this).attr("src", stillImg)
-      })
+      gifDiv.prepend(gifImg);
+      gifImg.attr("src", gif);
+      gifImg.attr("dispImg", stillImg);
+      gifImg.attr("dispGif", gif);
+      gifImg.attr("displaying", "gif");
+      gifImg.addClass("mediaItem");
+
       $("#gifs-appear-here").prepend(gifDiv);
     }
   });
+});
+
+$(document).on("click", ".mediaItem", function() {
+  console.log("image clicked");
+  if ($(this).attr("displaying") === "gif") {
+    $(this).attr("src", $(this).attr("dispIMG"));
+    $(this).attr("displaying", "img");
+  } else {
+    $(this).attr("src", $(this).attr("dispGif"));
+    $(this).attr("displaying", "gif");
+  }
 });
 
 $("#searchform").on("click", function(event) {
